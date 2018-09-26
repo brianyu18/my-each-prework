55d8020549c91364961b5fa33c88eb9cbf986100
brianyu18 <brianyu1990@gmail.com>
2018-09-26 23:01:53 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -5,24 +5,6 @@ def my_each (array)
       i = i + 1
     end
   end
-
-def eachmethod()
-
-def hello_t(array)
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-    
-    array
-  else
-    puts "Hey! No block was given!"
-  end
+  array
 end
 
-hello_t([arr]) do |num|
-  puts num
-end
